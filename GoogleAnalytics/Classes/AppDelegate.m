b3045ba376164d5fc0da845ebb25028dfacc2f14
Garrett Hall <garrett@tapad.com>
2013-06-14 15:18:56 -0400

    add underscore

--- GoogleAnalytics/Classes/AppDelegate.m
+++ GoogleAnalytics/Classes/AppDelegate.m
@@ -1,7 +1,6 @@
 //
-//  AppDelegate.m
+// Use this AppDelegate.m to send analytics with Tapestry
 //
-
 #import "AppDelegate.h"
 #import "GANTracker.h"
 #import "TapestryRequestBuilder.h"
@@ -12,44 +11,46 @@ static NSDate *lastAnalyticsSend;
 
 @synthesize window = window_;
 
-- (void)applicationDidFinishLaunching:(UIApplication *)application {
-    [[GANTracker sharedTracker] setDebug:true];
-    
-    [[GANTracker sharedTracker] startTrackerWithAccountID:@"UA-41283710-1"
-                                           dispatchPeriod:10
-                                                 delegate:nil];
-    if (lastAnalyticsSend == nil)
+- (void)sendAnalytics {
+    if (lastAnalyticsSend == nil) {
+        // initialize
+        [TapestryRequestBuilder registerAppWithPartnerId:@"12345"];
+        [[GANTracker sharedTracker] startTrackerWithAccountID:@"UA-41283710-1"
+                                               dispatchPeriod:10
+                                                     delegate:nil];
         lastAnalyticsSend = [NSDate date];
-    
+    }
+
     NSString* isNewSession = [lastAnalyticsSend timeIntervalSinceNow] < (-30 * 60) ? @"true" : @"false";
     TapestryRequestBuilder* req = [[TapestryRequestBuilder alloc] init];
     [req setAnalytics:[NSDictionary dictionaryWithObjectsAndKeys:isNewSession, @"isNewSession", nil]];
     [req sendWithCallback:^(NSDictionary *response) {
         NSDictionary* analytics = [[NSDictionary alloc] initWithDictionary:[response objectForKey:@"analytics"]];
         if ([analytics count] != 0) {
+            // You can modify the custom variables and scope here (2 = session-level scope)
             [[GANTracker sharedTracker] setCustomVariableAtIndex:1
-                                                            name:@"Visited Platforms"
+                                                            name:@"Visited_Platforms"
                                                            value:[[analytics objectForKey:@"vp"] description]
                                                            scope:kGANSessionScope
                                                        withError:NULL];
             [[GANTracker sharedTracker] setCustomVariableAtIndex:2
-                                                            name:@"Platforms Associated"
+                                                            name:@"Platforms_Associated"
                                                            value:[[analytics objectForKey:@"pa"] description]
                                                            scope:kGANSessionScope
                                                        withError:NULL];
             [[GANTracker sharedTracker] setCustomVariableAtIndex:3
-                                                            name:@"Platform Types"
+                                                            name:@"Platform_Types"
                                                            value:[[analytics objectForKey:@"pt"] description]
                                                            scope:kGANSessionScope
                                                        withError:NULL];
             [[GANTracker sharedTracker] setCustomVariableAtIndex:4
-                                                            name:@"First Visited Platform"
+                                                            name:@"First_Visited_Platform"
                                                            value:[[analytics objectForKey:@"fvp"] description]
                                                            scope:kGANSessionScope
                                                        withError:NULL];
             if ([analytics objectForKey:@"movp"] != nil)
                 [[GANTracker sharedTracker] setCustomVariableAtIndex:5
-                                                                name:@"Most Often Visited Platform"
+                                                                name:@"Most_Often_Visited_Platform"
                                                                value:[[analytics objectForKey:@"movp"] description]
                                                                scope:kGANSessionScope
                                                            withError:NULL];
@@ -63,7 +64,11 @@ static NSDate *lastAnalyticsSend;
             [[GANTracker sharedTracker] dispatch];
         }
     }];
-    
+}
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+    [[GANTracker sharedTracker] setDebug:true];
+    [self sendAnalytics];
     [window_ makeKeyAndVisible];
 }
 
@@ -72,5 +77,4 @@ static NSDate *lastAnalyticsSend;
     [window_ release];
     [super dealloc];
 }
-
 @end
