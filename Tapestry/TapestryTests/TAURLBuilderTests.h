69a9f04c997f8b8d2ce714ab95ff4e5b8cee64fa
Toby Matejovsky <toby.matejovsky@gmail.com>
2013-07-29 16:29:10 -0400

    Some tests for TAURLBuilder

---
+++ Tapestry/TapestryTests/TAURLBuilderTests.h
@@ -0,0 +1,17 @@
+//
+//  TAURLBuilderTests.h
+//  Tapestry
+//
+//  Created by Toby Matejovsky on 7/29/13.
+//  Copyright (c) 2013 Tapad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <SenTestingKit/SenTestingKit.h>
+#import "TAURLBuilder.h"
+
+
+@interface TAURLBuilderTests : SenTestCase {
+}
+
+@end
