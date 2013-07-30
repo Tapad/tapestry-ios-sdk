1390c2b86a2863bc101ae41f8513f47cc221a42f
Toby Matejovsky <toby.matejovsky@gmail.com>
2013-07-30 18:36:08 -0400

    TAURLBuilder documentation

--- Tapestry/Tapestry/TAURLBuilder.h
+++ Tapestry/Tapestry/TAURLBuilder.h
@@ -8,10 +8,26 @@
 
 #import <Foundation/Foundation.h>
 
+/** This class simplifies the gradual construction of a URL. */
 @interface TAURLBuilder : NSObject
+
+/** The current URL */
 @property (nonatomic, strong) NSMutableString* url;
 
+/** Creates and returns a URL builder with the given base URL.
+
+  @param baseURL The base URL
+  @return URL Builder
+ */
 + (TAURLBuilder*)urlBuilderWithBaseURL:(NSString*)baseURL;
+
+/** Appends a query parameter to the URL with the given name and value.
+
+  The value is URL-encoded.
+
+  @param name The name of the query parameter.
+  @param value The value of the query parameter.
+ */
 - (void)appendParameterNamed:(NSString*)name withValue:(NSString*)value;
 
 @end
