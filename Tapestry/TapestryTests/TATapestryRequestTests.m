ee3cbe7a9d00bb71c955a5c5f9e2f8ef4af8af43
Toby Matejovsky <toby.matejovsky@gmail.com>
2013-07-30 16:11:02 -0400

    Map, array, and flag based tests

--- Tapestry/TapestryTests/TATapestryRequestTests.m
+++ Tapestry/TapestryTests/TATapestryRequestTests.m
@@ -9,11 +9,34 @@
 #import "TATapestryRequestTests.h"
 #import "TAURLHelper.h"
 
+@interface TATapestryRequestTests ()
+
+@property (nonatomic, copy) NSString *key1;
+@property (nonatomic, copy) NSString *val1;
+@property (nonatomic, copy) NSString *key2;
+@property (nonatomic, copy) NSString *val2;
+@property (nonatomic, copy) NSString *mapAsEncodedJson;
+@property (nonatomic, copy) NSString *mapAsCsv;
+@property (nonatomic, copy) NSString *arrayAsEncodedJson;
+@property (nonatomic, copy) NSString *arrayAsCsv;
+
+@end
+
+
 @implementation TATapestryRequestTests
 
 - (void)setUp
 {
     [super setUp];
+
+    self.key1 = @"k1";
+    self.val1 = @"v1";
+    self.key2 = @"k2";
+    self.val2 = @"v2";
+    self.mapAsEncodedJson = [[NSString stringWithFormat:@"{\"%@\":\"%@\",\"%@\":\"%@\"}", self.key1, self.val1, self.key2, self.val2] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    self.mapAsCsv = [NSString stringWithFormat:@"%@:%@,%@:%@", self.key1, self.val1, self.key2, self.val2];
+    self.arrayAsEncodedJson = [[NSString stringWithFormat:@"[\"%@\",\"%@\"]", self.val1, self.val2] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    self.arrayAsCsv = [NSString stringWithFormat:@"%@,%@", self.val1, self.val2];
 
     request = [TATapestryRequest request];
 }
@@ -22,39 +45,118 @@
 {
     request = nil;
 
+    self.key1 = nil;
+    self.val1 = nil;
+    self.key2 = nil;
+    self.val2 = nil;
+    self.mapAsEncodedJson = nil;
+    self.mapAsCsv = nil;
+    self.arrayAsEncodedJson = nil;
+    self.arrayAsCsv = nil;
+
     [super tearDown];
 }
 
 - (void)testAddData
 {
-    id key = @"k";
-    id val = @"v";
-    
-    // Backend support encoded json map or a comma-separated list of key-value pairs.
-    id encodedJson = [[NSString stringWithFormat:@"{\"%@\":\"%@\"}", key, val] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    id csv = @"k=v";
-    
-    [request addData:val forKey:key];
-    
-    NSDictionary *params = [TAURLHelper paramsFromQuery:[request query]];
-    
-    // TODO use the one which is correct based on the TATapestryRequest implementation.
-    STAssertEqualObjects([params objectForKey:@"ta_add_data"], csv, @"Expected comma-separated list of key-value pairs");
-    STAssertEqualObjects([params objectForKey:@"ta_add_data"], encodedJson, @"Expected encoded json map");
+    [request addData:self.val1 forKey:self.key1];
+    [request addData:self.val2 forKey:self.key2];
+    [self assertMapMatches:@"ta_add_data"];
 }
 
-//- (void)addData:(NSString*)data forKey:(NSString*)key;
-//- (void)removeData:(NSString*)data forKey:(NSString*)key;
-//- (void)setData:(NSString*)data forKey:(NSString *)key;
-//- (void)clearData:(NSString *)dataKeys, ...;
-//- (void)addUniqueData:(NSString*)data forKey:(NSString*)key;
-//- (void)addAudiences:(NSString *)audiences, ...;
-//- (void)removeAudiences:(NSString *)audiences, ...;
-//- (void)listDevices;
-//- (void)setDepth:(NSInteger)depth;
-//- (void)setPartnerId:(NSString*)partnedId;
-//- (void)addUserId:(NSString*)userId forSource:(NSString*)source;
-//- (void)setStrength:(NSInteger)strength;
-//- (void)addTypedId:(NSString*)typedId forSource:(NSString*)source;
+- (void)testRemoveData
+{
+    [request removeData:self.val1 forKey:self.key1];
+    [request removeData:self.val1 forKey:self.key1];
+    [self assertMapMatches:@"ta_remove_data"];
+}
+
+- (void)testSetData
+{
+    [request setData:self.val1 forKey:self.key1];
+    [request setData:self.val2 forKey:self.key2];
+    [self assertMapMatches:@"ta_set_data"];
+}
+
+- (void)testClearData
+{
+    [request clearData:self.val1, self.val2];
+    [self assertArrayMatches:@"ta_clear_data"];
+}
+
+- (void)testAddUniqueData
+{
+    [request addUniqueData:self.val1 forKey:self.key1];
+    [request addUniqueData:self.val2 forKey:self.key2];
+    [self assertMapMatches:@"ta_sadd_data"];
+}
+
+- (void)testAddAudiences
+{
+    [request addAudiences:self.val1, self.val2];
+    [self assertArrayMatches:@"ta_add_audiences"];
+}
+
+- (void)testRemoveAudiences
+{
+    [request removeAudiences:self.val1, self.val2];
+    [self assertArrayMatches:@"ta_remove_audiences"];
+}
+
+- (void)testListDevices;
+{
+    [request listDevices];
+    [self assertFlagPresent:@"ta_list_devices"];
+}
+
+- (void)testSetDepth
+{
+    STFail(@"todo");
+}
+
+- (void)testSetPartnerId
+{
+    STFail(@"todo");
+}
+
+- (void)testAddUserId
+{
+    STFail(@"todo");
+}
+
+- (void)testSetStrength
+{
+    STFail(@"todo");
+}
+
+- (void)testAddTypedId
+{
+    STFail(@"todo");
+}
+
+// Helpers which use this class's various properties (key1, val1, request, etc).
+// When these are called, it's assumed that both values have been added (and both keys, if required).
+- (void)assertMapMatches:(NSString*)key
+{
+    NSDictionary *params = [TAURLHelper paramsFromQuery:[request query]];
+    STAssertEqualObjects([params objectForKey:key], self.mapAsCsv, @"Expected comma-separated list of key-value pairs");
+    STAssertEqualObjects([params objectForKey:key], self.mapAsEncodedJson, @"Expected encoded json map");
+}
+
+- (void)assertArrayMatches:(NSString*)key
+{
+    NSDictionary *params = [TAURLHelper paramsFromQuery:[request query]];
+    STAssertEqualObjects([params objectForKey:key], self.arrayAsCsv, @"Expected comma-separated list of values");
+    STAssertEqualObjects([params objectForKey:key], self.arrayAsEncodedJson, @"Expected encoded json array");
+}
+
+- (void)assertFlagPresent:(NSString*)key
+{
+    NSDictionary *params = [TAURLHelper paramsFromQuery:[request query]];
+    NSNumber* flagVal = [NSNumber numberWithBool:YES]; // from TAURLHelper. TODO move this into the interface.
+    STAssertEqualObjects([params objectForKey:key], flagVal, @"Expected key to be present");
+}
+
+// END test helpers.
 
 @end
