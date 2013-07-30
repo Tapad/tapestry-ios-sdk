9c022530e82f15f9d8f6991e6af4e6f2fe27a76d
Toby Matejovsky <toby.matejovsky@gmail.com>
2013-07-30 11:45:04 -0400

    Initial documentation and script

---
+++ generate-documentation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# Generates documentation for this project and moves it into the ./doc directory.
+#
+
+# appledoc executable is required.
+which appledoc &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "appledoc is not installed. Exiting."
+  exit 1
+fi
+
+# Move to the correct base directory.
+srcBaseDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $srcBaseDir
+
+appledoc --project-name "Tapestry iOS SDK" --project-company "Tapad" --company-id com.tapad.tapestry --create-html --keep-undocumented-objects --output . .
+
+rm -rf ./doc
+
+docBaseDir=$(cat docset-installed.txt | awk '/Path/ { print $2 }')
+
+htmlDocDir="$docBaseDir/Contents/Resources/Documents"
+
+cp -R $htmlDocDir doc
