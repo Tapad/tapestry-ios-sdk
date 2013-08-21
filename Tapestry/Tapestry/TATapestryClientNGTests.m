0cff42c45cc31bdafa383b8fe27a0007aa93f68a
Toby Matejovsky <toby.matejovsky@gmail.com>
2013-08-21 18:20:55 -0400

    Stub for client tests

---
+++ Tapestry/Tapestry/TATapestryClientNGTests.m
@@ -0,0 +1,13 @@
+//
+//  TATapestryClientNGTests.m
+//  Tapestry
+//
+//  Created by Toby Matejovsky on 8/21/13.
+//  Copyright (c) 2013 Tapad. All rights reserved.
+//
+
+#import "TATapestryClientNGTests.h"
+
+@implementation TATapestryClientNGTests
+
+@end
