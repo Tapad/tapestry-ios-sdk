71c54828de6b51f9688500d8adad7ae18d15eae8
Toby Matejovsky <toby.matejovsky@gmail.com>
2013-08-23 18:59:02 -0400

    TATapestryResponse documentation

--- Tapestry/Tapestry/TATapestryResponse.h
+++ Tapestry/Tapestry/TATapestryResponse.h
@@ -8,15 +8,56 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ The response type which is passed into a Tapestry request's callback.
+
+ It contains data returned by the API for the requesting device and its associated devices (as determined by the request).
+ */
 @interface TATapestryResponse : NSObject
 
+/**
+ Create and return a TATapestryResponse from the given dictionary.
+ @param dictionary Dictionary which contains the data from an API request
+ @return Tapestry response
+ */
 + (TATapestryResponse*) responseWithDictionary:(NSDictionary*)dictionary;
+
+/**
+ @return Whether or not the request has any API-level errors, such as permission violations.
+ */
 - (BOOL)wasSuccess;
+
+/**
+ @return Dictionary of typed IDs. Key is the ID type and the object is a list of ID values.
+ */
 - (NSDictionary*)getIds;
+
+/**
+ @return Dictionary of data. Objects are a list of values for the given key in chronological order.
+ */
 - (NSDictionary*)getData;
+
+/**
+ @return Array of audience tags.
+ */
 - (NSArray*)getAudiences;
+
+/**
+ @return Array of human-readable platforms, such as iPhone, Computer, AndroidTablet.
+ */
 - (NSArray*)getPlatforms;
+
+/**
+ @return Array of API errors, such as permission violations.
+ */
 - (NSArray*)getErrors;
+
+/**
+  Get the devices which were the source for the data in this class.
+  Each device is an NSDictionary which can be fed back into the constructor to access the data more easily.
+
+  @return Array of individual devices from the originating request.
+ */
 - (NSArray*)getDevices;
 
 @end
