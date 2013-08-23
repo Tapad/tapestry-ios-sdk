e79b234bb3d7588233feb67af5106f873484486a
Toby Matejovsky <toby.matejovsky@gmail.com>
2013-08-22 22:22:30 -0400

    Really add the new TapestryDemo

---
+++ TapestryDemo/TapestryDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TapestryDemo
+//
+//  Created by Toby Matejovsky on 8/22/13.
+//  Copyright (c) 2013 TapAd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TapestryDemo/TapestryDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TapestryDemo
+//
+//  Created by Toby Matejovsky on 8/22/13.
+//  Copyright (c) 2013 TapAd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
